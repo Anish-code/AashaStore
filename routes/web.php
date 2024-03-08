39cf1ca010d7777f7fb75953582ad146371dad56
Anish-code <anishmaharjan2057@gmail.com>
2024-03-09 01:47:43 +0545

    Admin Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\AdminLoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::group([ 'prefix' => 'admin' ], function(){
+    Route::group(['middleware' => 'admin.guest'], function(){
+        Route::get('/login', [AdminLoginController::class, 'index'])->name('admin.login');
+        Route::post('/authenticate', [AdminLoginController::class, 'authenticate'])->name('admin.authenticate');
+
+
+    });
+    Route::group(['middleware' => 'admin.auth'], function(){
+
+    });
+});
+require __DIR__.'/auth.php';
